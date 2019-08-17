555d85d79598fddab9e57b863f3e47e83657baf6
Echchouik <yassine.echchouik@gmail.com>
2019-08-17 15:09:00 +0200

    Nav-bar and setting routes
    Adding the navigation  bar and setting up the configuration for first route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    { enableTracing: true } // <-- debugging purposes only
+    )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
